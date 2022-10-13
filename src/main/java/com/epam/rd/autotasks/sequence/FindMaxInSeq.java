fb6fdf4b99daeed81acc4631ff9abaa8c4706b6a
Sofiia <sofiakazzz@gmail.com>
2022-10-13 14:10:22 +0300

    The program that reads a sequence of integer values from standard output and finds the maximum value.

--- src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
+++ src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
@@ -1,20 +1,30 @@
 package com.epam.rd.autotasks.sequence;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FindMaxInSeq {
     public static int max() {
 
-        // Put your code here
-
-        return 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = 1;
+        int element = 0;
+        Scanner x = new Scanner(System.in);
+        while (n != 0) {
+            n = x.nextInt();
+            list.add(n);
+            element = list.get(0);
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i) > element && list.get(i) != 0) {
+                    element = list.get(i);
+                }
+            }
+        }
+        return element;
     }
 
     public static void main(String[] args) {
-
         System.out.println("Test your code here!\n");
-
-        // Get a result of your code
-
         System.out.println(max());
     }
 }
